4d9e6aec4dcb0522f86b834b84cc40dad5d1ed29
phiaseitz <sophia.seitz@students.olin.edu>
2015-12-11 11:54:43 -0500

    Styling changes

--- js/visualizer.js
+++ js/visualizer.js
@@ -1,9 +1,170 @@
 // Create the Google Map…
 var map = new google.maps.Map(d3.select("#map").node(), {
-  zoom: 5,
+  zoom: 4,
   center: new google.maps.LatLng(39.828215,-98.5817593),
-  mapTypeId: google.maps.MapTypeId.HYBRID
+  mapTypeId: google.maps.MapTypeId.ROADMAP
 });
+
+var styles = [
+  //This colors the land.
+  {
+    featureType: "landscape",
+    elementType: "all",
+    stylers: [
+      {
+        hue: "#669999"
+      },
+      {
+        saturation: -10
+      },
+      {
+        lightness: 0
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  //Points of interest are parks and stuff.
+  //We can turn this off
+  {
+    featureType: "poi",
+    elementType: "all",
+    stylers: [
+      {
+        hue: "#ffffff"
+      },
+      {
+        saturation: -100
+      },
+      {
+        lightness: 100
+      },
+      {
+        visibility: "off"
+      }
+    ]
+  },
+  //the roads themselves
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        hue: "#bbc0c4"
+      },
+      {
+        saturation: -93
+      },
+      {
+        lightness: 31
+      },
+      {
+        visibility: "simplified"
+      },
+      {
+        weight: 2
+      }
+    ]
+  },
+  //The road labels
+  {
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      {
+        hue: "#bbc0c4"
+      },
+      {
+        saturation: -93
+      },
+      {
+        lightness: 31
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.arterial",
+    elementType: "labels",
+    stylers: [
+      {
+        hue: "#bbc0c4"
+      },
+      {
+        saturation: -93
+      },
+      {
+        lightness: -2
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "road.local",
+    elementType: "geometry",
+    stylers: [
+      {
+        hue: "#e9ebed"
+      },
+      {
+        saturation: -90
+      },
+      {
+        lightness: -8
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  },
+  {
+    featureType: "transit",
+    elementType: "all",
+    stylers: [
+      {
+        hue: "#e9ebed"
+      },
+      {
+        saturation: 10
+      },
+      {
+        lightness: 69
+      },
+      { 
+        visibility: "on"
+      }
+    ]
+  },
+  //This changes the color of the water
+  {
+    featureType: "water",
+    elementType: "all",
+    stylers: [
+      {
+        hue: "#6699ff"
+      },
+      {
+        saturation: 100
+      },
+      {
+        lightness: 20
+      },
+      {
+        visibility: "simplified"
+      }
+    ]
+  }
+];
+
+map.setOptions({styles: styles});
+
+// Load the station data. When the data comes back, create an overlay.
+d3.csv("csv_data.csv", function(data) {
 
 const COLORS = d3.scale.linear()
   .domain([0, 1])
