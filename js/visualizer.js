81e9dac6f8cd076927de33fcd58f7a047b23ae30
phiaseitz <sophia.seitz@students.olin.edu>
2015-12-15 23:52:28 -0500

    actually fixed the bug I thought I fixed before

--- js/visualizer.js
+++ js/visualizer.js
@@ -609,6 +609,7 @@ function updateDonutChart(target, datum={}, criteria=[]) {
 
   //Add new radius and angle information here. 
   criteriaGroups.each(function(d,i){
+    d.datum = datum;
     if (isUpdatingRadius) {
       d.normedValue = evaluateDatum(datum, [d.data]);
       d.newOuter = radiusScale(d.normedValue);
